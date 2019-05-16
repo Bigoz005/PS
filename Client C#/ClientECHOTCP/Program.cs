85505ea5f824127fa4a5962c5e6a23d4ceae0989
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2019-05-16 13:22:26 +0200

    Client i Serwer C#

---
+++ Client C#/ClientECHOTCP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientECHOTCP
+{
+    class TcpEchoClient
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting echo client...");
+
+            int port = 0;
+
+            Console.Write("Port: ");
+            port = Convert.ToInt32(Console.ReadLine());
+            TcpClient client = new TcpClient("localhost", port);
+
+            NetworkStream stream = client.GetStream();           
+
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+            Console.WriteLine("Connection established.");
+
+            while (true)
+            {
+                Console.Write("Enter text to send: ");
+                string lineToSend = Console.ReadLine();
+                Console.WriteLine("Sending to server: " + lineToSend);
+                writer.WriteLine(lineToSend);
+                if(lineToSend == "end") { break; }
+                string lineReceived = reader.ReadLine();
+                Console.WriteLine("Received from server: " + lineReceived);
+            }
+
+            stream.Close();
+            stream.Dispose();
+        }
+    }
+}
---
+++ Server C#/ServerECHOTCP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace ServerECHOTCP
+{
+    public class TcpEchoServer
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Starting echo server...");
+
+            int port = 0;
+            Console.Write("Port: ");
+            port = Convert.ToInt32(Console.ReadLine()); ;
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+            StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+            var clientPort = ((IPEndPoint)client.Client.RemoteEndPoint).Port;
+            while (true)
+            {
+                Console.WriteLine("Client connected with IP {0} on port {1}", ((IPEndPoint)client.Client.RemoteEndPoint).Address, port);
+                string inputLine = "";
+                while (inputLine != null)
+                {
+                    inputLine = reader.ReadLine();
+                    if (inputLine == "end") { break; }
+                    writer.WriteLine("Echoing string: " + inputLine);
+                    Console.WriteLine("Echoing string: " + inputLine);
+                }
+                if (inputLine == "end") { break; }
+            }
+            Console.WriteLine("Server saw disconnect from client.");
+            listener.Stop();
+            stream.Close();
+            stream.Dispose();
+        }
+    }
+}

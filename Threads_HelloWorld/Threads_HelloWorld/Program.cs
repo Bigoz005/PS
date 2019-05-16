3cc095953347e97e0439acf334d5d7d33aa557c4
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2019-05-16 13:57:22 +0200

    Thread_HelloWorld

---
+++ Threads_HelloWorld/Threads_HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿// C# program to illustrate the creation 
+// of thread using static method 
+using System;
+using System.Threading;
+
+public class ExThread
+{
+    // Static method for thread a 
+    public static void thread1()
+    {
+            Console.WriteLine("Hello World");   
+    }
+
+}
+
+// Driver Class 
+public class GFG
+{
+
+    // Main method 
+    public static void Main()
+    {
+        // Creating and initializing threads 
+        Thread a = new Thread(ExThread.thread1);
+        a.Start();
+
+        a.Join();
+        Console.WriteLine("Joined");
+        System.Threading.Thread.Sleep(5000);
+    }
+}
